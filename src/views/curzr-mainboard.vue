ec5ee5a4c75df12384eb7f2df76d570d1ab3ab40
Taylon Chan <taylonchan@beNovelty.com>
2022-05-13 03:05:14 +0800

    init project structure

---
+++ src/views/curzr-mainboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="mainboard" class="mainboard">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main-board',
+    components: {
+      
+    },
+    computed: {
+      
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
