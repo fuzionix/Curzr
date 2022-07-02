f0dcf8324244b05a95e3de5bb9e6f362a993dffd
Taylon Chan <tchan199616@gamil.com>
2022-07-03 03:13:56 +0800

    update edit model

--- src/components/cursor-model-edit.vue
+++ src/components/cursor-model-edit.vue
@@ -8,13 +8,29 @@
       <img src="../assets/icon/arrow.svg" alt="view code arrow" width="10" height="10">
     </normal-button>
     <h5 class="title">Edit Cursor</h5>
-    <small>Cursor Size</small>
-    <range-bar
-      id="edit-range-size"
-      class="range-bar"
-      :range-value="0"
-      :minmax="[-25, 25]"
-    />
+    <div class="edit-container">
+      <div class="input-container">
+        <small class="input-name">Size</small>
+        <small class="input-description">Edit the size of the cursor</small>
+        <range-bar
+          id="edit-size"
+          class="range-bar"
+          :range-value="0"
+          :minmax="[-25, 25]"
+        />
+      </div>
+      <div class="input-container">
+        <small class="input-name">Delay</small>
+        <small class="input-description">Edit the dalay time of the cursor</small>
+        <range-bar
+          id="edit-delay"
+          class="range-bar"
+          :range-value="0"
+          :minmax="[-25, 25]"
+        />
+      </div>
+    </div>
+    
   </section>
 </template>
 
@@ -53,7 +69,6 @@
 @import '../style/main.scss';
 
 .cursor-model-edit {
-  width: 300px;
   
   .normal-btn {
     border: 1px solid $--section-line-color;
@@ -76,17 +91,37 @@
   }
 
   .title {
-    margin-top: 1rem;
+    margin: 1rem 1rem 0;
     letter-spacing: .5px;
     font-size: 1.5rem;
     font-variation-settings: 'wght' $--bold;
   }
 
-  .range-bar {
-    max-width: initial;
-    margin: 1rem;
-    margin-top: 1rem;
+  .edit-container {
+    padding: 1rem;
+
+    .input-container {
+      display: flex;
+      flex-direction: column;
+      
+      .input-name { 
+        opacity: .75;
+      }
+
+      .input-description { 
+        opacity: .5;
+      }
+
+      .range-bar {
+        max-width: initial;
+        margin: 1rem 0 2rem;
+      }
+    }
+
+    
   }
+
+  
 }
 </style>
 
